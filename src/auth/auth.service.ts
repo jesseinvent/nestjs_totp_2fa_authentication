287858c2f5542085d6ec4a6def041d712d19c2dc
Jesse Ojih <jcinvent05@gmail.com>
2022-05-04 22:58:25 +0100

    Updates

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { SignupDto } from './dto/signup.dto';
 import * as bcrypt from 'bcryptjs';
 import { LoginDto } from './dto/login.dto';
 import * as speakeasy from 'speakeasy';
+import * as qrcode from 'qrcode';
 
 @Injectable()
 export class AuthService {
@@ -47,21 +48,26 @@ export class AuthService {
       throw new ForbiddenException('User does not exists');
     }
 
+    if (user.twoFA_enabled) {
+      throw new BadRequestException('2FA already enabled on account');
+    }
+
     const secret = this.generateSecret();
 
     await this.userService.update(email, { twoFA_secret: secret.base32 });
 
-    return { key: secret.base32 };
+    // Generate QRcode
+    const url = await qrcode.toDataURL(secret.otpauth_url);
+
+    return { key: secret.base32, qrcode_url: url };
   }
 
   generateSecret() {
-    const secret = speakeasy.generateSecret();
+    const secret = speakeasy.generateSecret({ name: 'Invent App' });
 
-    console.log(secret);
+    const { hex, base32, otpauth_url } = secret;
 
-    const { hex, base32 } = secret;
-
-    return { hex, base32 };
+    return { hex, base32, otpauth_url };
   }
 
   async complete2FASetup(email: string, token: string) {
@@ -69,6 +75,14 @@ export class AuthService {
 
     if (!user) {
       throw new ForbiddenException('User does not exists');
+    }
+
+    if (!user.twoFA_secret) {
+      throw new BadRequestException('2FA enabling request not made');
+    }
+
+    if (user.twoFA_enabled) {
+      throw new BadRequestException('2FA already enabled on account');
     }
 
     const verify2FAToken = this.verify2FAOtp(user.twoFA_secret, token);
@@ -81,7 +95,7 @@ export class AuthService {
 
     return {
       status: true,
-      message: '2FA enable for on account account',
+      message: '2FA successfully enabled on account account',
     };
   }
 
